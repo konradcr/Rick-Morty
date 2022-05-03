d2631481a9f8eb3e90291075ffff130eee1f63b6
konradcr <cureau.konrad@icloud.com>
2022-05-03 19:04:23 +0200

    Initial Commit

---
+++ Rick&Morty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick&Morty
+//
+//  Created by Konrad Cureau on 03/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
